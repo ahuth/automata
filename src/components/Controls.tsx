8d645c1d32b76908242b50065b8a5ed5e14ef84a
Andrew Huth <andrew@huth.me>
2020-01-05 22:18:10 -0800

    Add input for the number of rows

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import uniqueId from '../utils/uniqueId';
 
 export default class Controls extends React.Component {
-  id = uniqueId();
+  ruleId = uniqueId();
+  rowsId = uniqueId();
 
   render() {
     return (
       <section aria-label="Cellular automata controls">
-        <label htmlFor={this.id}>Rule</label>
-        <input id={this.id} />
+        <label htmlFor={this.ruleId}>Rule</label>
+        <input id={this.ruleId} type="number" min="0" max="255" />
+
+        <label htmlFor={this.rowsId}>Number of rows</label>
+        <input id={this.ruleId} type="number" min="0" max="50" />
+
         <button type="button">Draw</button>
       </section>
     );
