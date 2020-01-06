b9745b371b942c4f83a750e7ac8d0de33455885e
Andrew Huth <andrew@huth.me>
2020-01-05 22:24:17 -0800

    Accept props for the controls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import Controls from './Controls';
 export default function App() {
   return (
     <React.Fragment>
-      <Controls />
+      <Controls rows={25} rule={30} onDrawClick={() => console.log('clicked')} />
       <hr />
     </React.Fragment>
   );
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import uniqueId from '../utils/uniqueId';
 
-export default class Controls extends React.Component {
+type Byte = number;
+
+type Props = {
+  onDrawClick: () => void,
+  rows: number,
+  rule: Byte,
+}
+
+export default class Controls extends React.Component<Props> {
   ruleId = uniqueId();
   rowsId = uniqueId();
 
   render() {
+    const { onDrawClick, rows, rule } = this.props;
+
     return (
       <section aria-label="Cellular automata controls">
         <label htmlFor={this.ruleId}>Rule</label>
-        <input id={this.ruleId} type="number" min="0" max="255" />
+        <input id={this.ruleId} type="number" min="0" max="255" value={rule} />
 
         <label htmlFor={this.rowsId}>Number of rows</label>
-        <input id={this.ruleId} type="number" min="0" max="50" />
+        <input id={this.ruleId} type="number" min="0" max="50" value={rows} />
 
-        <button type="button">Draw</button>
+        <button onClick={onDrawClick} type="button">Draw</button>
       </section>
     );
   }
