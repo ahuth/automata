f1effce1a93e25590a7232c6448c4c2a2fac9697
Andrew Huth <andrew@huth.me>
2020-01-12 15:01:12 -0800

    Add some info about "interesting" rules

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,6 +9,9 @@ export default function App() {
   return (
     <React.Fragment>
       <Controls dispatch={dispatch} rules={state.rules} />
+      <aside>
+        <p>Some of the more interesting rules are: 30, 54, 60, 62, 90, 94, 102, 110, 122, 126, 150, 158, 182, 188, 190, 222</p>
+      </aside>
       <hr />
       <Grid rows={state.rows} />
     </React.Fragment>
