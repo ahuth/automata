ab9f2ca7106889d49ea6e52e7cbd3feef96c61dd
Andrew Huth <andrew@huth.me>
2020-01-11 20:08:14 -0800

    Start out with rule 30

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -8,12 +8,12 @@ type Props = {
 export default function Controls({ onDrawClick }: Props) {
   const rules = [
     { inputs: [1, 1, 1], output: 0 },
-    { inputs: [1, 1, 0], output: 0 },
+    { inputs: [1, 1, 0], output: 1 },
     { inputs: [1, 0, 1], output: 0 },
-    { inputs: [1, 0, 0], output: 0 },
-    { inputs: [0, 1, 1], output: 0 },
+    { inputs: [1, 0, 0], output: 1 },
+    { inputs: [0, 1, 1], output: 1 },
     { inputs: [0, 1, 0], output: 0 },
-    { inputs: [0, 0, 1], output: 0 },
+    { inputs: [0, 0, 1], output: 1 },
     { inputs: [0, 0, 0], output: 0 },
   ];
 
