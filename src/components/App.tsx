8f0e6592c27afccc177302613a2f00567092d351
Andrew Huth <andrew@huth.me>
2020-01-11 20:13:51 -0800

    Lift the initial rules up to the App-level

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import Controls from './Controls';
 
 export default function App() {
+  const rules = [
+    { inputs: [1, 1, 1], output: 0 },
+    { inputs: [1, 1, 0], output: 1 },
+    { inputs: [1, 0, 1], output: 0 },
+    { inputs: [1, 0, 0], output: 1 },
+    { inputs: [0, 1, 1], output: 1 },
+    { inputs: [0, 1, 0], output: 0 },
+    { inputs: [0, 0, 1], output: 1 },
+    { inputs: [0, 0, 0], output: 0 },
+  ];
+
   return (
     <React.Fragment>
-      <Controls onDrawClick={() => console.log('clicked')} />
+      <Controls rules={rules} onDrawClick={() => console.log('clicked')} />
       <hr />
     </React.Fragment>
   );
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import RuleSelector from './RuleSelector';
 
+type Rule = {
+  inputs: number[],
+  output: number,
+};
+
 type Props = {
   onDrawClick: () => void,
+  rules: Rule[],
 }
 
-export default function Controls({ onDrawClick }: Props) {
-  const rules = [
-    { inputs: [1, 1, 1], output: 0 },
-    { inputs: [1, 1, 0], output: 1 },
-    { inputs: [1, 0, 1], output: 0 },
-    { inputs: [1, 0, 0], output: 1 },
-    { inputs: [0, 1, 1], output: 1 },
-    { inputs: [0, 1, 0], output: 0 },
-    { inputs: [0, 0, 1], output: 1 },
-    { inputs: [0, 0, 0], output: 0 },
-  ];
-
+export default function Controls({ onDrawClick, rules }: Props) {
   return (
     <section aria-label="Cellular automata controls">
       <RuleSelector rules={rules} />
