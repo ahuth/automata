a8555ecb18cfe449a651e70d2e2df40c88362a87
Andrew Huth <andrew@huth.me>
2020-12-12 17:54:23 -0800

    Use a mapped object type to reduce duplication

--- src/automata/rule.ts
+++ src/automata/rule.ts
@@ -3,14 +3,7 @@ export type Byte = [Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit];
 export type Position = 0b111 | 0b110 | 0b101 | 0b100 | 0b011 | 0b010 | 0b001 | 0b000;
 
 type Rule = {
-  0b111: Bit;
-  0b110: Bit;
-  0b101: Bit;
-  0b100: Bit;
-  0b011: Bit;
-  0b010: Bit;
-  0b001: Bit;
-  0b000: Bit;
+  [position in Position]: Bit;
 }
 
 export type Type = Rule;
